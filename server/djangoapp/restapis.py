6973d1b50d84b36f6e83394b91ed12ca7133b6f1
Krishna <krishnaitdb@email.com>
2024-11-06 16:03:25 -0500

    5th commit

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -11,12 +11,34 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
-# def get_request(endpoint, **kwargs):
+def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
+    params = ""
+    if kwargs:
+        for key, value in kwargs.items():
+            params = params + key + "=" + value + "&"
+    request_url = backend_url + endpoint + "?" + params
+    print("GET from {} ".format(request_url))
+    try:
+        response = requests.get(request_url)
+        return response.json()
+    except Exception:
+        print("Network failure occurred")
 
-# def analyze_review_sentiments(text):
-# request_url = sentiment_analyzer_url+"analyze/"+text
+def analyze_review_sentiments(text):
 # Add code for retrieving sentiments
+    request_url = sentiment_analyzer_url+"analyze/"+text
+    try:
+        response = requests.get(request_url)
+        return response.json()
+    except Exception:
+        print("Network failure occurred")
 
-# def post_review(data_dict):
+def post_review(data_dict):
 # Add code for posting review
+    request_url = backend_url + "/insert_review"
+    try:
+        response = requests.post(request_url, json=data_dict)
+        return response.json()
+    except Exception:
+        print("Network failure occurred")
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -80,17 +80,48 @@ def registration(request):
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
-# def get_dealerships(request):
-# ...
+def get_dealerships(request, state="All"):
+    if(state == "All"):
+        endpoint = "/fetchDealers"
+    else:
+        endpoint = "/fetchDealers/"+state
+    dealerships = get_request(endpoint)
+    return JsonResponse({"status":200,"dealers":dealerships})
+
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
-# def get_dealer_reviews(request,dealer_id):
-# ...
+def get_dealer_reviews(request,dealer_id):
+    if (dealer_id):
+        endpoint = "/fetchReviews/dealer/"+str(dealer_id)
+        reviews = get_request(endpoint)
+        for dealer_review in reviews:
+            response = analyze_review_sentiments(dealer_review['review'])
+            dealer_review['sentiment'] = response['sentiment']
+        return JsonResponse({"status":200,"reviews":reviews})
+    else:
+        return JsonResponse({"status":400,"message":"Bad Request"})
+
 
 # Create a `get_dealer_details` view to render the dealer details
-# def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request, dealer_id):
+    if (dealer_id):
+        endpoint = "/fetchDealer/" + str(dealer_id)
+        dealership = get_request(endpoint)
+        return JsonResponse({"status": 200, "dealer": dealership})
+    else:
+        return JsonResponse({"status": 400, "message": "Bad Request"})
+
 
 # Create a `add_review` view to submit a review
-# def add_review(request):
-# ...
+def add_review(request):
+    if request.user.is_authenticated:
+        data = json.loads(request.body)
+        try:
+            response = post_review(data)
+            return JsonResponse({"status": 200, "message": response})
+        except Exception:
+            return JsonResponse(
+                {"status": 401, "message": "Error adding review"}
+            )
+    else:
+        return JsonResponse({"status": 403, "message": "Unauthorized"})
