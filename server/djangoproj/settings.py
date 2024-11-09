c201ecd4e0d9d552c702b381a43bfec12e627d0f
krishna <krishnaitdb@gmail.com>
2024-11-08 20:11:47 -0500

    10th commit

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,13 +31,13 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     (
-        'https://krishnaitdb-8000.theiadockernext-1-labs-prod-'
+        'https://krishnaitdb-8000.theiadockernext-0-labs-prod-'
         'theiak8s-4-tor01.proxy.cognitiveclass.ai'
     )
 ]
 CSRF_TRUSTED_ORIGINS = [
     (
-        'https://krishnaitdb-8000.theiadockernext-1-labs-prod-'
+        'https://krishnaitdb-8000.theiadockernext-0-labs-prod-'
         'theiak8s-4-tor01.proxy.cognitiveclass.ai'
     )
 ]
